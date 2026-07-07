57e27e324f704708b108ff9e38f839686c9bf24c
Pritam barik <barickpritam08@gmail.com>
2026-07-07 19:56:22 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Log from "./Pages/Log/Log";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
+       <Route path="/blog" element={<Log />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ Frontend/src/Pages/Log/Log.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BlogBreadcrumb from '../../Components/BlogBreadcrum/BlogBreadcrum'
+import Blog from '../../Components/Blog/Blog'
+
+const Log = () => {
+  return (
+    <div>
+        <BlogBreadcrumb />
+        <Blog /> 
+    </div>
+  )
+}
+
+export default Log
